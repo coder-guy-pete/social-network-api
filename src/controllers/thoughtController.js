904927564fa916e21d17bb19ad8e6265cd6cfa1a
Peter Hintze <hintze.peter@gmail.com>
2025-02-23 18:42:13 -0500

    Create thought route

--- src/controllers/thoughtController.js
+++ src/controllers/thoughtController.js
@@ -1,4 +1,4 @@
-import { Thought } from '../models/index.js';
+import { Thought, User } from '../models/index.js';
 
 export const getThoughts = async (_req, res) => {
     try {
@@ -19,6 +19,24 @@ export const getThoughtById = async (req, res) => {
         res.status(200).json(thought);
     } catch (err) {
         console.error(err);
+        res.status(400).json(err);
+    }
+};
+
+export const createThought = async (req, res) => {
+    try {
+        const { thoughtText, username, userId } = req.body;
+
+        if (!thoughtText || !username || !userId) {
+            return res.status(400).json({ message: 'Missing required fields' });
+        }
+
+        const thought = await Thought.create({ thoughtText, username });
+
+        await User.findByIdAndUpdate(userId, { $push: { thoughts: thought._id } });
+        
+        res.status(201).json(thought);
+    } catch (err) {
         res.status(400).json(err);
     }
 };
--- src/routes/api/thoughtRoutes.js
+++ src/routes/api/thoughtRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 const router = Router();
-import { getThoughts, getThoughtById } from '../../controllers/thoughtController.js';
+import { getThoughts, getThoughtById, createThought } from '../../controllers/thoughtController.js';
 
 // /api/thoughts
-router.route('/').get(getThoughts);
+router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:id
 router.route('/:id').get(getThoughtById);

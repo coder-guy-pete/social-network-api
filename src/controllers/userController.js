62444da9ba48bd1e93cda19fc2dd602acc02866d
Peter Hintze <hintze.peter@gmail.com>
2025-02-23 18:14:04 -0500

    add friend route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { User } from '../models/index.js';
+import { User, Thought } from '../models/index.js';
 
 export const getUsers = async (_req, res) => {
     try {
@@ -58,13 +58,37 @@ export const updateUser = async (req, res) => {
 
 export const deleteUser = async (req, res) => {
     try {
-        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        const userToDelete = await User.findById(req.params.id);
 
-        if (!deletedUser) {
+        if (!userToDelete) {
             return res.status(404).json({ message: 'User not found' });
         }
 
-        res.status(200).json({ message: 'User deleted successfully' });
+        await Thought.deleteMany({ username: userToDelete.username });
+        await User.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({ message: 'User and associated thoughts deleted successfully' });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+export const addFriend = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id);
+        const friend = await User.findById(req.params.friendId);
+
+        if (!user || !friend) {
+            return res.status(404).json({ message: 'User or friend not found' });
+        }
+
+        if (user.friends.includes(req.params.friendId)) {
+            return res.status(400).json({ message: 'Friend is already in friend list' });
+        }
+
+        user.friends.push(req.params.friendId);
+        await user.save();
+        res.status(200).json({ message: 'Friend added successfully' });
     } catch (err) {
         res.status(400).json(err);
     }

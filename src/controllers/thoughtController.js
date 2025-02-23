1d86db488b5d04cb2bfa07cfaafebf4eeefd8248
Peter Hintze <hintze.peter@gmail.com>
2025-02-23 18:31:23 -0500

    get thought by ID

--- src/controllers/thoughtController.js
+++ src/controllers/thoughtController.js
@@ -8,3 +8,17 @@ export const getThoughts = async (_req, res) => {
         res.status(400).json(err);
     }
 };
+
+export const getThoughtById = async (req, res) => {
+    try {
+        const thought = await Thought.findById(req.params.id);
+
+        if (!thought) {
+            return res.status(404).json({ message: 'Thought not found' });
+        }
+        res.status(200).json(thought);
+    } catch (err) {
+        console.error(err);
+        res.status(400).json(err);
+    }
+};
--- src/routes/api/thoughtRoutes.js
+++ src/routes/api/thoughtRoutes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 const router = Router();
-import { getThoughts } from '../../controllers/thoughtController.js';
+import { getThoughts, getThoughtById } from '../../controllers/thoughtController.js';
 
 // /api/thoughts
 router.route('/').get(getThoughts);
+
+// /api/thoughts/:id
+router.route('/:id').get(getThoughtById);
 
 export default router;
